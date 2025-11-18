bb9f65315000299a1132d0c0931ecd2881d14db6
merwanel <>
2025-11-18 21:54:23 +0100

    adding pinia

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import hljs from 'highlight.js/lib/core'
 import xml from 'highlight.js/lib/languages/xml'
@@ -7,6 +8,8 @@ import 'highlight.js/styles/atom-one-dark.css'
 
 hljs.registerLanguage('xml', xml)
 
-createApp(App)
-  .use(hljsVuePlugin)
-  .mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(hljsVuePlugin)
+app.mount('#app')
